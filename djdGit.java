f2b2682bbe182d70ac67c8416b3b04da41d3a757
DangD96 <35606061+DangD96@users.noreply.github.com>
2022-06-03 23:08:49 -0500

    Initial commit

---
+++ djdGit.java
@@ -0,0 +1,14 @@
+public class djdGit {
+    public int alwaysOne() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        djdGit myObj = new djdGit();
+        if (myObj.alwaysOne() == 1) {
+            System.out.println("true");
+        } else {System.out.println("false");}
+    }
+}
+
+
